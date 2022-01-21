b3f872f1b8ef50fb84714be072a0c813c1da573b
mamaredo_d <11kakeru30@gmail.com>
2022-01-21 19:21:49 +0900

    add: 読み込み時の地図の位置, マーカー, 任意の位置にマーカーを設置できるように

--- src/feature/Map/map-client.ts
+++ src/feature/Map/map-client.ts
@@ -1,49 +1,80 @@
-// const mapView = document.getElementById('map-view')!
-const setMarker = (map: google.maps.Map) => {
-  map.addListener('click', (e: any) => {
-    console.log(e)
-    new google.maps.Marker({
-      position: e.latLng,
-      map: map
-    })
+type Location = { lat: number; lng: number }
+const halLocation: Location = {
+  lat: 35.16832838570073,
+  lng: 136.88561516878792
+}
+
+const getInitLocation = async () => {
+  if (!navigator.geolocation) {
+    window.alert('Geolocation is not suport :(')
+    return halLocation
+  }
+
+  const currentPosition = await new Promise<Location>(res => {
+    navigator.geolocation.getCurrentPosition(
+      pos => {
+        const currentLocation = {
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude
+        }
+        res(currentLocation)
+      },
+      err => {
+        console.error(err.message)
+        res(halLocation)
+      }
+    )
+  })
+
+  return currentPosition
+}
+
+const setMarker = (options: google.maps.MarkerOptions) => {
+  return new google.maps.Marker({
+    ...options
   })
 }
 
-const mapClient = () => {
+type MarkerClient = {
+  markers: google.maps.Marker[]
+  disableMarker: () => void
+  markedCount: number
+  setMarker: google.maps.MarkerOptions
+}
+
+const markerClient: MarkerClient = {
+  markers: [],
+  disableMarker: function () {
+    console.log('marker', this.markers)
+    const marker = this.markers[this.markedCount - 1]
+    marker.setMap(null)
+  },
+
+  get markedCount() {
+    return this.markers.length
+  },
+
+  set setMarker(options: Parameters<typeof setMarker>[0]) {
+    this.markers[this.markedCount] = setMarker(options)
+  }
+}
+
+const handleMapClick = (map: google.maps.Map) => {
+  map.addListener('click', (e: google.maps.MapMouseEvent) => {
+    markerClient.disableMarker()
+    markerClient.setMarker = { map, position: e.latLng }
+  })
+}
+
+export const mapClient = async () => {
   const mapView = document.getElementById('map-view')!
+  const initLocation = await getInitLocation()
+
   const map = new google.maps.Map(mapView, {
     zoom: 13,
-    center: { lat: 36, lng: 139 }
+    center: initLocation
   })
+  markerClient.setMarker = { map, position: initLocation }
+
+  handleMapClick(map)
 }
-
-mapClient()
-
-// class MapClient {
-//   private map: any
-//   public view: any
-
-//   constructor() {
-//     this.view = document.getElementById('map-view')!
-//     this.map = new google.maps.Map(this.view, {
-//       zoom: 13,
-//       center: { lat: 36, lng: 139 }
-//     })
-//   }
-
-//   setMarker() {
-//     console.log('from set Marker')
-//     this.map.addListener('click', (e: { latLng: any }) => {
-//       new google.maps.Marker({
-//         position: e.latLng,
-//         map: this.map
-//       })
-//     })
-//   }
-// }
-
-// const mapClient = new MapClient()
-// export default mapClient
-
-// const mapClient = () => new MapClient()
-// export { mapClient, mapView }
