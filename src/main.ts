c8f4f505207b9085db66c8940d01aa69765194fd
mamaredo_n <11kakeru30@gmail.com>
2022-01-21 14:06:50 +0900

    update: map-clientの呼び出し

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,4 @@
 import './style.css'
+import './feature/Map/map-client'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
