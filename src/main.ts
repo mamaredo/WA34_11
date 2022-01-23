f54e289720ea91b52905cb81af79d321389f1158
mamaredo_d <11kakeru30@gmail.com>
2022-01-24 00:47:59 +0900

    add: weatherInfoをimport

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'ress'
 import './style.css'
 import { mapInit } from './feature/map/mapInit'
 import './feature/weather/api'
+import '@/feature/weather/weatherInfo'
 
 window.onload = () => {
   // const app = document.querySelector<HTMLDivElement>('#app')!
