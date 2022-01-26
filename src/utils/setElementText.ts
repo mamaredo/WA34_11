80d0e340eeb5e18764bc11e40fc17ba1eec63d0b
mamaredo_n <11kakeru30@gmail.com>
2022-01-26 14:49:55 +0900

    add: 定義したidのみhtmlタグをセットできるように

--- src/utils/setElementText.ts
+++ src/utils/setElementText.ts
@@ -1,8 +1,14 @@
 import { getElementById } from './getElementById'
 
+const renderHtml = 'weather-icon'
+
 export const setElementText = (id: string, text: string | number) => {
   const element = getElementById(id)
-  typeof text === 'number'
-    ? (element.textContent = String(text))
-    : (element.textContent = text)
+  if (renderHtml === id) {
+    element.innerHTML = text as string
+  } else {
+    typeof text === 'number'
+      ? (element.textContent = String(text))
+      : (element.textContent = text)
+  }
 }
